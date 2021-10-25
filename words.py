36d7ed56c1a9d8aae555e7ec371f85e98d35834d
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2021-10-24 22:26:06 -0700

    Six python exercises
    Six introductory python exercises for the Springboard software engineering course

---
+++ words.py
@@ -0,0 +1,24 @@
+def print_upper_words(words):
+    """Print each word on sep line, uppercased.
+
+        >>> print_upper_words(["Programming", "is", "pretty", "fun"])
+        PROGRAMMING
+        IS
+        PRETTY
+        FUN
+    """
+    for word in words:
+        print(word.upper().split())
+
+
+def print_upper_words2(words):
+    for word in words:
+        if word.startswith("e") or word.startswith("E"):
+            print(word.upper())
+
+def print_upper_words3(words, must_start_with):
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+
